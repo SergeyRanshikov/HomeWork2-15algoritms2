862edad90189a7cb312802905775051a53e4972d
ranss@rambler.ru <ranss@rambler.ru>
2023-07-20 16:14:19 +0300

    commit задание 2-16 алгоритмы 3

--- src/main/java/com/example/homework215algoritms2/IntegerListImpl.java
+++ src/main/java/com/example/homework215algoritms2/IntegerListImpl.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 public class IntegerListImpl implements IntegerList {
 
     private static final int DEFAULT_CAPACITY = 10;
-    private final Integer[] storage;
+    private Integer[] storage;
     private int size;
 
     public IntegerListImpl() {
@@ -23,19 +23,10 @@ public class IntegerListImpl implements IntegerList {
         }
     }
 
-//    public IntegerListImpl() {
-//        this.storage = new Integer[DEFAULT_CAPACITY];
-//        this.size = 0;
-//    }
 
-//    public IntegerListImpl(int capacity) {
-//        this.storage = new Integer[capacity];
-//        this.size = 0;
-//    }
-
-    private void validateSize() {
+    private void growIfNeeded() {
         if (size == storage.length) {
-            throw new StorageIsFullException();
+            grow();
         }
     }
 
@@ -47,7 +38,7 @@ public class IntegerListImpl implements IntegerList {
 
     @Override
     public Integer add(Integer item) {
-        validateSize();
+        growIfNeeded();
         validateItem(item);
         storage[size++] = item;
         return item;
@@ -55,7 +46,7 @@ public class IntegerListImpl implements IntegerList {
 
     @Override
     public Integer add(int index, Integer item) {
-        validateSize();
+        growIfNeeded();
         validateItem(item);
         validateIndex(index);
 
@@ -110,7 +101,7 @@ public class IntegerListImpl implements IntegerList {
     @Override
     public boolean contains(Integer item) {
         Integer[] storageCopy = toArray();
-        sortInsertion(storageCopy);
+        sort(storageCopy);
         return binarySort(storageCopy, item);
     }
 
@@ -173,16 +164,43 @@ public class IntegerListImpl implements IntegerList {
         return Arrays.copyOf(storage, size);
     }
 
-        private void sortInsertion(Integer[] arr) {
-        for (int i = 1; i < arr.length; i++) {
-            int temp = arr[i];
-            int j = i;
-            while (j > 0 && arr[j - 1] >= temp) {
-                arr[j] = arr[j - 1];
-                j--;
-            }
-            arr[j] = temp;
+
+        private void sort(Integer[] arr) {
+        quickSort(arr,0,arr.length - 1);
+    }
+
+    private void quickSort(Integer[] arr, int begin, int end) {
+            if (begin < end) {
+                int partitionIndex = partition(arr, begin, end);
+
+                quickSort(arr, begin, partitionIndex - 1);
+                quickSort(arr, partitionIndex + 1, end);
+
         }
+
+    }
+
+    private int partition(Integer[] arr, int begin, int end) {
+        int pivot = arr[end];
+        int i = (begin - 1);
+
+        for (int j = begin; j < end; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+
+                swapElements(arr, i, j);
+            }
+        }
+
+        swapElements(arr, i + 1, end);
+        return i + 1;
+
+    }
+
+    private void swapElements(Integer[] arr, int i1, int i2) {
+        int temp = arr[i1];
+        arr[i1] = arr[i2];
+        arr[i2] = temp;
     }
 
 
@@ -205,4 +223,9 @@ public class IntegerListImpl implements IntegerList {
         }
         return false;
     }
+
+    private void grow() {
+        storage = Arrays.copyOf(storage, size + size / 2);
+    }
+
 }
